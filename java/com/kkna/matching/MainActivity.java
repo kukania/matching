9e73fc7cbb8c9ea64b3068658e8658f9d52d92c5
kukania <angkswnstn@naver.com>
2017-01-03 16:23:34 +0900

    set interface

--- java/com/kkna/matching/MainActivity.java
+++ java/com/kkna/matching/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ java/com/kkna/matching/matching/MatchingComponent.java
@@ -0,0 +1,7 @@
+package com.kkna.matching.matching;
+
+/**
+ * Created by angks on 2017-01-03.
+ */
+public interface MatchingComponent {
+}
